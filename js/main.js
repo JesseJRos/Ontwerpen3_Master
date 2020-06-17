dc3104888d704863caa8374def34badd03916321
Jesse R <37974966+JesseJRos@users.noreply.github.com>
2020-06-17 15:22:31 +0200

    AOS library, signs update and final adjustments

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
-//   1 kopieer de hele heutemeut
-//   2 vervolgens zet je een id op het element wat je wilt triggeren
-//   3 daarna geef je het id bij isVisible en offScreen
-//   4 geef dat id ook mee aan if statements 'getElementById'
-//   5 bedenk een nieuwe id naam die die animatie triggered
-//   6 zet die id in beide if statements
-//   7 maak vervolgens de id aan in je css en style hem
-
 // FundFamily sign
-document.addEventListener('scroll', function (e) {
+window.addEventListener('scroll', function (e) {
     var top  = window.pageYOffset + window.innerHeight,
         isVisible = top > document.getElementById('idFundFamily').offsetTop,
         offScreen = top < document.getElementById('idFundFamily').offsetTop;
@@ -19,51 +11,9 @@ document.addEventListener('scroll', function (e) {
         document.getElementById('idFundFamily').classList.remove('animateFundFamily');
      }
   });
-  
-// Polaroid 1
-  document.addEventListener('scroll', function (e) {
-    var top  = window.pageYOffset + window.innerHeight,
-        isVisible = top > document.getElementById('animatedPolaroid1').offsetTop,
-        offScreen = top < document.getElementById('animatedPolaroid1').offsetTop;
-  
-     if (isVisible) {
-       document.getElementById('animatedPolaroid1').classList.add('animatePolaroid1');
-     }
-     if (offScreen) {
-        document.getElementById('animatedPolaroid1').classList.remove('animatePolaroid1');
-     }
-  });
-
-  // Polaroid 2
-  document.addEventListener('scroll', function (e) {
-    var top  = window.pageYOffset + window.innerHeight,
-        isVisible = top > document.getElementById('animatedPolaroid2').offsetTop,
-        offScreen = top < document.getElementById('animatedPolaroid2').offsetTop;
-  
-     if (isVisible) {
-       document.getElementById('animatedPolaroid2').classList.add('animatePolaroid2');
-     }
-     if (offScreen) {
-        document.getElementById('animatedPolaroid2').classList.remove('animatePolaroid2');
-     }
-  });
-
-  // Polaroid 3
-  document.addEventListener('scroll', function (e) {
-    var top  = window.pageYOffset + window.innerHeight,
-        isVisible = top > document.getElementById('animatedPolaroid3').offsetTop,
-        offScreen = top < document.getElementById('animatedPolaroid3').offsetTop;
-  
-     if (isVisible) {
-       document.getElementById('animatedPolaroid3').classList.add('animatePolaroid3');
-     }
-     if (offScreen) {
-        document.getElementById('animatedPolaroid3').classList.remove('animatePolaroid3');
-     }
-  });
 
 // Sign Petitions
-document.addEventListener('scroll', function (e) {
+window.addEventListener('scroll', function (e) {
     var top  = window.pageYOffset + window.innerHeight,
         isVisible = top > document.getElementById('idSignPetitions').offsetTop,
         offScreen = top < document.getElementById('idSignPetitions').offsetTop;
@@ -76,16 +26,61 @@ document.addEventListener('scroll', function (e) {
      }
   });
 
-  // Bottom Image
-document.addEventListener('scroll', function (e) {
-    var top  = window.pageYOffset + window.innerHeight,
-        isVisible = top > document.getElementById('idBottomImage').offsetTop,
-        offScreen = top < document.getElementById('idBottomImage').offsetTop;
+
+// OBSOLETE (but kept for reference)
+
+// Polaroid 1
+// window.addEventListener('scroll', function (e) {
+//     var top  = window.pageYOffset + window.innerHeight,
+//         isVisible = top > document.getElementById('animatedPolaroid1').offsetTop,
+//         offScreen = top < document.getElementById('animatedPolaroid1').offsetTop;
   
-     if (isVisible) {
-       document.getElementById('idBottomImage').classList.add('animateBottomImage');
-     }
-     if (offScreen) {
-        document.getElementById('idBottomImage').classList.remove('animateBottomImage');
-     }
-  });
+//      if (isVisible) {
+//        document.getElementById('animatedPolaroid1').classList.add('animatePolaroid1');
+//      }
+//      if (offScreen) {
+//         document.getElementById('animatedPolaroid1').classList.remove('animatePolaroid1');
+//      }
+//   });
+
+  // Polaroid 2
+//   window.addEventListener('scroll', function (e) {
+//     var top  = window.pageYOffset + window.innerHeight,
+//         isVisible = top > document.getElementById('animatedPolaroid2').offsetTop,
+//         offScreen = top < document.getElementById('animatedPolaroid2').offsetTop;
+  
+//      if (isVisible) {
+//        document.getElementById('animatedPolaroid2').classList.add('animatePolaroid2');
+//      }
+//      if (offScreen) {
+//         document.getElementById('animatedPolaroid2').classList.remove('animatePolaroid2');
+//      }
+//   });
+
+  // Polaroid 3
+//   window.addEventListener('scroll', function (e) {
+//     var top  = window.pageYOffset + window.innerHeight,
+//         isVisible = top > document.getElementById('animatedPolaroid3').offsetTop,
+//         offScreen = top < document.getElementById('animatedPolaroid3').offsetTop;
+  
+//      if (isVisible) {
+//        document.getElementById('animatedPolaroid3').classList.add('animatePolaroid3');
+//      }
+//      if (offScreen) {
+//         document.getElementById('animatedPolaroid3').classList.remove('animatePolaroid3');
+//      }
+//   });
+
+  // Bottom Image
+//   window.addEventListener('scroll', function (e) {
+//     var top  = window.pageYOffset + window.innerHeight,
+//         isVisible = top > document.getElementById('idBottomImage').offsetTop,
+//         offScreen = top < document.getElementById('idBottomImage').offsetTop;
+  
+//      if (isVisible) {
+//        document.getElementById('idBottomImage').classList.add('animateBottomImage');
+//      }
+//      if (offScreen) {
+//         document.getElementById('idBottomImage').classList.remove('animateBottomImage');
+//      }
+//   });
